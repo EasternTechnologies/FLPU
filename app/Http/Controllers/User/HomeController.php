079ca30f133be635bc09e1b392838ed5bc362775
pavel <mail@east-tech.by>
2019-03-05 17:31:48 +0300

    search

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -76,11 +76,11 @@ class HomeController extends Controller
 
 	    if($request->ajax()){
 
-		    $q = $request->q;
+		    $q = str_replace(['{','}','[',']'], '',$request->q);
 
 	    } else {
 
-		    $q = strip_tags($request->q);
+		    $q = strip_tags(str_replace(['{','}','[',']'], '',$request->q));
 
 	    }
 
